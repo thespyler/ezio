1cb8e9570fd14c0e2b0303992657bf945841def9
Spyler <alkatalankar@gmail.com>
2020-04-28 16:38:19 +0530

    version 1.0

---
+++ login/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+
+
+def hello(request):
+    return render(request, 'main.html')
+
+def blog1(request):
+    return render(request, 'blog1.html')
+# Create your views here.
+
+def blog2(request):
+    return render(request, 'blog2.html')
+
+def next(request):
+    name = request.POST['user']
+    # password = request.POST['pass']
+    # if password == 'thespyler':
+    #     # HttpResponse('Hello' + name)
+    HttpResponseRedirect('/ezio/', {'name': name})
+    # else:
+    #     HttpResponse('wrong password!')
